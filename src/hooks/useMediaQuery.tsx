c58bc985388e734adf1e465990ccc38a3a6b1e3f
1zumisawashun <shun.1zumisawa@gmail.com>
2022-06-12 19:35:13 +0900

    メディアクエリのフックを作成

---
+++ src/hooks/useMediaQuery.tsx
@@ -0,0 +1,46 @@
+import styled from "@emotion/styled";
+
+const breakpoints = {
+  iphone5: 320,
+  sp: 576,
+  tab: 768,
+  pc: 1042,
+} as const;
+
+/**
+ * deviceの方が直感的に使いやすいので下記を採用する
+ * deviceもmediaQueryも補完（予測変換）できている
+ * https://github.com/emotion-js/emotion/blob/main/docs/media-queries.mdx
+ */
+export const device = {
+  iphone5: `(min-width: ${breakpoints.iphone5}px)`,
+  sp: `(min-width: ${breakpoints.sp}px)`,
+  tab: `(min-width: ${breakpoints.tab}px)`,
+  pc: `(min-width: ${breakpoints.pc}px)`,
+};
+
+// eslint-disable-next-line
+const FirstTextWrapper = styled("div")`
+  margin: auto;
+  @media ${device.tab} {
+    max-width: 800px;
+  }
+  @media ${device.pc} {
+    max-width: 1400px;
+  }
+`;
+
+export const mediaQuery = (key: keyof typeof breakpoints) =>
+  `@media (min-width: ${breakpoints[key]}px)`;
+
+// eslint-disable-next-line
+const SecondTestWrapper = styled("div")`
+  margin: auto;
+  color: "green";
+  ${mediaQuery("iphone5")}: {
+    color: "gray";
+  }
+  ${mediaQuery("sp")}: {
+    color: "hotpink";
+  }
+`;
