639b7a7cd3f31988d8e5f8990aebc99eb9849d8c
1zumisawashun <shun.1zumisawa@gmail.com>
2022-07-06 23:40:15 +0900

    dev環境でCIが動いているか確認する

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -13,7 +13,7 @@ const Header: React.VFC = () => {
         {!isOpen && (
           <li className="logo">
             <StoreButton onClick={() => history.push("/")} />
-            <span>Temanashi Master</span>
+            <span>Temanashi Develop</span>
           </li>
         )}
       </ul>
