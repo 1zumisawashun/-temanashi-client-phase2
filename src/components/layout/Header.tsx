4ddb14ce291b3a2c33d439cdff1353717e0497f2
1zumisawashun <shun.1zumisawa@gmail.com>
2022-07-06 15:28:06 +0900

    CircleCIを起こす

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -13,7 +13,7 @@ const Header: React.VFC = () => {
         {!isOpen && (
           <li className="logo">
             <StoreButton onClick={() => history.push("/")} />
-            <span>Temanashi Client</span>
+            <span>Temanashi Client Phase2</span>
           </li>
         )}
       </ul>
