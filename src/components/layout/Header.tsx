455bb30d8da70e903ea2ad218a7b4b64566f68bb
1zumisawashun <shun.1zumisawa@gmail.com>
2022-07-06 15:14:51 +0900

    デプロイが反映されているかの確認

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -13,7 +13,7 @@ const Header: React.VFC = () => {
         {!isOpen && (
           <li className="logo">
             <StoreButton onClick={() => history.push("/")} />
-            <span>Temanashi</span>
+            <span>Temanashi Client</span>
           </li>
         )}
       </ul>
