7c799c29cf88ee4a9b2b3e88a68fa060830c34da
1zumisawashun <shun.1zumisawa@gmail.com>
2022-07-06 22:11:33 +0900

    CI環境で分けてみる

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -13,7 +13,7 @@ const Header: React.VFC = () => {
         {!isOpen && (
           <li className="logo">
             <StoreButton onClick={() => history.push("/")} />
-            <span>Temanashi Client CI Test</span>
+            <span>Temanashi Master</span>
           </li>
         )}
       </ul>
