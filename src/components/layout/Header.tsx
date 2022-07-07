b23ca96880e5aa8f9be0f0072a179d0ffa209e48
1zumisawashun <shun.1zumisawa@gmail.com>
2022-07-08 00:47:01 +0900

    ヘッダーの変更

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -13,7 +13,7 @@ const Header: React.VFC = () => {
         {!isOpen && (
           <li className="logo">
             <StoreButton onClick={() => history.push("/")} />
-            <span>temanashi</span>
+            <span>temanashi develop</span>
           </li>
         )}
       </ul>
